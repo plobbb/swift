841ea535e0573dccad2bd39c588dad13eb7bc021
Rintaro Ishizaki <fs.output@gmail.com>
2016-07-29 23:45:24 +0900

    [SE-0101] Add test cases for migration fix-its.

--- test/expr/expressions.swift
+++ test/expr/expressions.swift
@@ -872,3 +872,27 @@ let _ = (x, 3).1
 (x,y).1 = 7 // expected-error {{cannot assign to immutable expression of type 'Int'}}
 x = (x,(3,y)).1.1
 
+
+// SE-0101 sizeof family functions are reconfigured into MemoryLayout
+protocol Pse0101 {
+  associatedtype Value
+}
+class Cse0101<U> {
+  typealias T = U
+  var val: U { fatalError() }
+}
+func se0101<P: Pse0101>(x: Cse0101<P>) {
+  // Note: The first case is actually not allowed, but it is very common and can be compiled currently.
+  _ = sizeof(P) // expected-error {{'sizeof' is unavailable: use MemoryLayout<T>.size instead.}} {{7-14=MemoryLayout<}} {{15-16=>.size}} {{none}}
+                // expected-warning@-1 {{missing '.self' for reference to metatype of type 'P'}}
+  _ = sizeof(P.self) // expected-error {{'sizeof' is unavailable: use MemoryLayout<T>.size instead.}} {{7-14=MemoryLayout<}} {{15-21=>.size}} {{none}}
+  _ = sizeof(P.Value.self) // expected-error {{'sizeof' is unavailable: use MemoryLayout<T>.size instead.}} {{7-14=MemoryLayout<}} {{21-27=>.size}} {{none}}
+  _ = sizeof(Cse0101<P>.self) // expected-error {{'sizeof' is unavailable: use MemoryLayout<T>.size instead.}} {{7-14=MemoryLayout<}} {{24-30=>.size}} {{none}}
+  _ = sizeofValue(x) // expected-error {{'sizeofValue' is unavailable: use MemoryLayout.of(value).size instead.}} {{7-19=MemoryLayout.of(}} {{20-21=).size}} {{none}}
+  _ = sizeofValue(x.val) // expected-error {{'sizeofValue' is unavailable: use MemoryLayout.of(value).size instead.}} {{7-19=MemoryLayout.of(}} {{24-25=).size}} {{none}}
+
+  _ = alignof(Cse0101<P>.T.self) // expected-error {{'alignof' is unavailable: use MemoryLayout<T>.alignment instead.}} {{7-15=MemoryLayout<}} {{27-33=>.alignment}} {{none}}
+  _ = strideof(P.Type.self) // expected-error {{'strideof' is unavailable: use MemoryLayout<T>.stride instead.}} {{7-16=MemoryLayout<}} {{22-28=>.stride}} {{none}}
+  _ = alignofValue(x) // expected-error {{'alignofValue' is unavailable: use MemoryLayout.of(value).alignment instead.}} {{7-20=MemoryLayout.of(}} {{21-22=).alignment}} {{none}}
+  _ = strideofValue(x) // expected-error {{'strideofValue' is unavailable: use MemoryLayout.of(value).stride instead.}} {{7-21=MemoryLayout.of(}} {{22-23=).stride}} {{none}}
+}
